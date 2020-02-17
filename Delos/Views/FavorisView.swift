4126d68f30d3d69db29042f592f0f9534a0b5d3a
joachimalencon <joachim.a@hotmail.fr>
2020-02-17 14:40:50 +0100

    creation favorisiez et appel dans tab bar

---
+++ Delos/Views/FavorisView.swift
@@ -0,0 +1,21 @@
+//
+//  Favoris.swift
+//  Delos
+//
+//  Created by Joachim Alencon on 17/02/2020.
+//  Copyright © 2020 Carneiro Jorge. All rights reserved.
+//
+
+import SwiftUI
+
+struct Favoris: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Favoris_Previews: PreviewProvider {
+    static var previews: some View {
+        Favoris()
+    }
+}
