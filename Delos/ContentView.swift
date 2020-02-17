a5180f2b97b72945154c729352d77587f3d7369e
Jorge <carneiro092@MacBook-Pro-26.local>
2020-02-17 14:04:36 +0100

    initial commit

---
+++ Delos/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Delos
+//
+//  Created by Carneiro Jorge on 17/02/2020.
+//  Copyright © 2020 Carneiro Jorge. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
