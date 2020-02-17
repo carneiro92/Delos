60e9468b422d06d68e1dc972b27f30cadb9366bc
Jorge Carneiro <jorgecarneiro092@gmail.com>
2020-02-17 17:24:35 +0100

    J-1

--- Delos/Views/MapView.swift
+++ Delos/Views/MapView.swift
@@ -7,15 +7,23 @@
 //
 
 import SwiftUI
+import MapKit
 
-struct MapView: View {
-    var body: some View {
-        Text("Hello, Map!")
+struct MapView: UIViewRepresentable {
+    func makeUIView(context : Context) -> MKMapView {
+        MKMapView(frame : .zero)
+    }
+    func updateUIView(_ view: MKMapView, context: Context) {
+        let coordinate = CLLocationCoordinate2D(latitude: 45.764043, longitude: 4.835659)
+        let span  = MKCoordinateSpan(latitudeDelta: 2.0, longitudeDelta: 2.0)
+        let region = MKCoordinateRegion(center: coordinate, span: span)
+        view.setRegion(region , animated : true)
     }
 }
-
+#if DEBUG
 struct MapView_Previews: PreviewProvider {
     static var previews: some View {
         MapView()
     }
 }
+#endif
