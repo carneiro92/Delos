bb626b7eb8dd53b4c0d23d57a8836631f43926fc
DamienC <damien.cesar.dc@gmail.com>
2020-02-18 11:09:37 +0100

    Envoie de données Firebase Implémentation getter de données

---
+++ Delos/Extensions/CodableAsDictionary.swift
@@ -0,0 +1,21 @@
+//
+//  CodableAsDictionary.swift
+//  Delos
+//
+//  Created by Damien Cesar on 18/02/2020.
+//  Copyright © 2020 Carneiro Jorge. All rights reserved.
+//
+
+import Foundation
+import Firebase
+
+extension Encodable {
+  func asDictionary() throws -> [String: Any] {
+    let data = try JSONEncoder().encode(self)
+    guard let dictionary = try JSONSerialization.jsonObject(with: data, options: .allowFragments) as? [String: Any] else {
+      throw NSError()
+    }
+    print("Donnée envoyé \(dictionary)")
+    return dictionary
+  }
+}
