f4d7127db41077eda854845ad56ad0f2ec15be44
DamienC <damien.cesar.dc@gmail.com>
2020-02-20 17:30:23 +0100

    Implémentation Profile

--- Delos/Views/ProfileView.swift
+++ Delos/Views/ProfileView.swift
@@ -9,10 +9,36 @@
 import SwiftUI
 
 struct ProfileView: View {
+    @State var listResa: [ReservationData] = []
+    
     var body: some View {
-        Text("Hello, Profile!")
+        VStack {
+            HStack{
+                Image(systemName: "person.circle").resizable()
+            }.frame(width: 150, height: 150)
+            VStack{
+                Text("Nom")
+                Text("Prénom")
+                Text("Email")
+                Text("Tel")
+            }
+            HStack{
+                Text("Réservation a venir :").font(.title)
+            }
+            VStack {
+                List(listResa) { item in
+                    Text("Test")
+                }
+            }.frame(height: 150)
+        }
+                .onAppear {
+                    GetReservationUser() { resas in
+                        self.listResa = resas
+                    }
+                }
     }
 }
+
 
 struct ProfileView_Previews: PreviewProvider {
     static var previews: some View {
